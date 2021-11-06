ba34cd6dab1ae889cbe3fafc8f28fe783968e6ac
xTheQAZx <dhruv.agarwals@gmail.com>
2021-11-05 22:01:04 -0500

    Update TestingFile.py

--- TestingFile.py
+++ TestingFile.py
@@ -11,19 +11,24 @@ pygame.display.set_icon(icon)
 # Planets
 planet_redblue1 = pygame.image.load('planet_redblue.png')
 planet_redblue1 = pygame.transform.scale(planet_redblue1, (50, 50))
+planet_redblueX = 370
+planet_redblueY = 480
 
 planet_frozen1 = pygame.image.load('planet_frozen.png')
 planet_frozen1 = pygame.transform.scale(planet_frozen1, (50, 50))
-
+planet_frozenX = 370
+planet_frozenY = 400
 
 def planet_redblue():
-    screen.blit(planet_redblue1, (370, 480))
+    screen.blit(planet_redblue1, (planet_redblueX, planet_redblueY))
 
 def planet_frozen():
-    screen.blit(planet_frozen1, (370, 400))
+    screen.blit(planet_frozen1, (planet_frozenX, planet_frozenY))
 
 running = True
 while running:
+    planet_redblueX=planet_redblueX+1
+    planet_frozenY=planet_frozenY-1
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
