b9a2e17aa45597ed61ac9c177b302cf051cc0c51
jckcob <jacobnt03@gmail.com>
2021-11-05 23:01:35 -0500

    "Final" space obj commit
    Optimized main screen

--- TestingFile.py
+++ TestingFile.py
@@ -13,15 +13,21 @@ bg = pygame.image.load("bg.jpeg")
 bg = pygame.transform.scale(bg, (800,600))
 
 # Planets
+
+sun1 = pygame.image.load('sun.png')
+sun1 = pygame.transform.scale(sun1, (150, 150))
+sunX = 300
+sunY = 200
+
 planet_redblue1 = pygame.image.load('planet_redblue.png')
 planet_redblue1 = pygame.transform.scale(planet_redblue1, (50, 50))
 planet_redblueX = 370
 planet_redblueY = 480
 
-planet_frozen1 = pygame.image.load('planet_frozen.png')
-planet_frozen1 = pygame.transform.scale(planet_frozen1, (50, 50))
-planet_frozenX = 370
-planet_frozenY = 400
+planet_green1 = pygame.image.load('planet_green.png')
+planet_green1 = pygame.transform.scale(planet_green1, (50, 50))
+planet_greenX = 370
+planet_greenY = 400
 
 planet_ring = pygame.image.load('planet_ring.png')
 planet_ring = pygame.transform.scale(planet_ring, (75, 50))
@@ -31,11 +37,14 @@ planet_ringY = 480
 def background():
     screen.blit(bg, (0,0))
 
+def sun():
+    screen.blit(sun1, (sunX, sunY))
+
 def planet_redblue():
     screen.blit(planet_redblue1, (planet_redblueX, planet_redblueY))
 
 def planet_frozen():
-    screen.blit(planet_frozen1, (planet_frozenX, planet_frozenY))
+    screen.blit(planet_green1, (planet_greenX, planet_greenY))
 
 def planet_cracked():
     screen.blit(planet_ring, (planet_ringX, planet_ringY))
@@ -44,11 +53,12 @@ running = True
 while running:
     screen.fill((0,0,0))
     planet_redblueX=planet_redblueX+1
-    planet_frozenY=planet_frozenY-1
+    planet_greenY=planet_greenY-1
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    background()       
+    background()
+    sun()     
     planet_redblue()
     planet_frozen()
     planet_cracked()
