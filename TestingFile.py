4202c0a09fad0e389c0360c8b98b137201d6c62c
xTheQAZx <63619126+xTheQAZx@users.noreply.github.com>
2021-11-05 21:20:20 -0500

    Update TestingFile.py

--- TestingFile.py
+++ TestingFile.py
@@ -4,8 +4,16 @@ pygame.init()
 
 screen = pygame.display.set_mode((800,600))
 
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('astronaut.jpeg')
+pygame.display.set_icon(icon)
+
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            
+    screen.fill((0,150,0))
+    pygame.display.update()
