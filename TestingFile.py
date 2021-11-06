44e5a4e195fb945c6b0b1b07d677d09f0fa8d2d1
bondecai <35945575+bondecai@users.noreply.github.com>
2021-11-05 21:49:37 -0500

    added 2 planets

--- TestingFile.py
+++ TestingFile.py
@@ -9,12 +9,21 @@ icon = pygame.image.load('astronaut.jpg')
 pygame.display.set_icon(icon)
 
 # Planets
-planetImg = pygame.image.load('astronaut.jpeg')
-planetX = 370
-planetY = 480
+planet_redblue1 = pygame.image.load('planet_redblue.png')
+planet_redblue1 = pygame.transform.scale(planet_redblue1, (50, 50))
+planet_redblueX = 370
+planet_redblueY = 480
 
-def planet():
-    screen.blit(planetImg, (planetX, planetY))
+planet_frozen1 = pygame.image.load('planet_frozen.png')
+planet_frozen1 = pygame.transform.scale(planet_frozen1, (50, 50))
+planet_frozenX = 370
+planet_frozenY = 400
+
+def planet_redblue():
+    screen.blit(planet_redblue1, (planet_redblueX, planet_redblueY))
+
+def planet_frozen():
+    screen.blit(planet_frozen1, (planet_frozenX, planet_frozenY))
 
 running = True
 while running:
@@ -22,5 +31,6 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             
-    planet()
+    # planet_redblue()
+    planet_frozen()
     pygame.display.update()
